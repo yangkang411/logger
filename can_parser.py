ff3c4720f370360c120c33078d9d0c11209fda37
ysong-aceinna <ysong@aceinna.com>
2020-11-27 16:35:17 +0800

    Re-construct can_reader. ACS, BLF and can test log can be parsed and convert to csv format data.

---
+++ can_parser.py
@@ -0,0 +1,218 @@
+# coding: utf-8
+"""
+General CAN messages Parser.
+
+Created on 2020-10-29
+@author: Ocean
+
+"""
+
+import os
+import sys
+import math
+from enum import Enum
+import datetime
+
+
+class PGNType(Enum):
+    SSI2       = 61481   # Slope Sensor Information 2
+    ARI        = 61482   # Angular Rate
+    ACCS       = 61485   # Acceleration Sensor
+    CCVS1      = 65265   # Cruise Control/Vehicle Speed 1, to get Wheel-Based Vehicle Speed
+    GEAR       = 61445   # 'Transmission Selected Gear' in 'Electronic Transmission Controller' message.
+    WSI        = 65215   # Wheel Speed Information   
+    TACHOGRAPH = 65132   # Tachograph
+
+
+class CANParser():
+    def __init__(self):
+        pass
+
+    def parse_PDU(self, PDU):
+        '''
+        function: get Priority, PGN, PF, PS, SA info from PDU.
+
+        in: int, PDU, eg 0x0cf029a2
+        '''
+        priority = (PDU >> 26) & 7  # 0b0111
+        PF = (PDU >> 16) & 255      # 0b11111111
+        PS = (PDU >> 8) & 255       # 0b11111111
+        SA = PDU & 255              # 0b11111111
+
+        if PF < 240:
+            PGN = PF * 256
+        else:
+            PGN = PF * 256 + PS
+        # print('{0},{1},{2},{3},{4},{5}'.format(hex(PDU), priority, PGN, PF, PS, SA))
+        return (priority, PGN, PF, PS, SA)
+
+    def generate_PDU(self, Priority, PGN, SA):
+        # PDU = Priority << 26 + PGN << 8 + SA
+        PDU =  Priority * math.pow(2, 26) + PGN * math.pow(2, 8) + SA
+        return int(PDU)
+
+    def parse_gyro(self, msg):
+        '''
+        Parse CAN msg to get Angular Rate data.
+
+        in: CAN msg
+        out: tuple, (gyro_x, gyro_y, gyro_z) in [deg/s]
+        '''
+        wx_uint = msg[0] + 256 * msg[1] 
+        wy_uint = msg[2] + 256 * msg[3] 
+        wz_uint =  msg[4] + 256 * msg[5] 
+        wx = wx_uint * (1/128.0) - 250.0
+        wy = wy_uint * (1/128.0) - 250.0
+        wz = wz_uint * (1/128.0) - 250.0
+        # print('WX: {0:3.2f} WY: {1:3.2f} WZ: {2:3.2f}'.format(wx,wy,wz))
+        return (wx, wy, wz)
+
+    def parse_accel(self, msg):
+        '''
+        Parse CAN msg to get Acceleration Sensor data.
+
+        in: CAN msg
+        out: tuple, (accel_x, accel_y, accel_z) in [m/s²]
+        '''
+        ax_uint = msg[0] + 256 * msg[1] 
+        ay_uint = msg[2] + 256 * msg[3] 
+        az_uint =  msg[4] + 256 * msg[5] 
+        ax = ax_uint * (0.01) - 320.0
+        ay = ay_uint * (0.01) - 320.0
+        az = az_uint * (0.01) - 320.0
+        # print('AX: {0:3.2f} AY: {1:3.2f} AZ: {2:3.2f}'.format(ax,ay,az))
+        return (ax, ay, az)
+
+    def parse_tilt(self, msg):
+        '''
+        Parse CAN msg to get tilt data.
+
+        in: CAN msg
+        out: tuple, (roll, pitch) in [deg]
+        '''
+        pitch_uint = msg[0] + 256 * msg[1] +  65536 * msg[2]
+        roll_uint = msg[3] + 256 * msg[4] +  65536 * msg[5]
+        pitch = pitch_uint * (1/32768) - 250.0
+        roll = roll_uint * (1/32768) - 250.0
+        # print('Roll: {0:3.2f} Pitch: {1:3.2f}'.format(roll,pitch))
+        return (roll, pitch)
+
+    def parse_velocity1(self, msg):
+        '''
+        Parse CAN msg (PGN 65265) to get Wheel-Based Vehicle Speed. Speed of the vehicle as calculated from wheel or tailshaft speed.
+
+        in: CAN msg
+        out: Wheel-Based Vehicle Speed in [km/h]
+        '''
+        vel_unint = msg[1] + 256 * msg[2] 
+        vel_km_perhr = vel_unint * (1/256.0)
+        vel_m_persec = vel_km_perhr / 3.6
+        # print('{0:3.2f}'.format(vel_km_perhr))
+        return (vel_km_perhr,)
+
+    def parse_velocity2(self, msg):
+        '''
+        Parse CAN msg (PGN 65215) to get Wheel Speed Information.
+
+        in: CAN msg
+        out: (front_axle_speed, front_left_wheel_speed, front_right_wheel_speed
+        rear_left1_wheel_speed, rear_right1_wheel_speed, rear_left2_wheel_speed,rear_right2_wheel_speed)
+        '''
+        offset = -7.8125 #km/h
+        # Front Axle Speed, the average speed of the two front wheels. range:[0, 250.996] km/h. 
+        front_axle_speed        = (msg[0] + 256 * msg[1]) / 256.0
+        # The speed of the front axle, left wheel speed, 
+        front_left_wheel_speed  = msg[2] / 16 + offset + front_axle_speed
+        # The speed of the front axle, right wheel speed, 
+        front_right_wheel_speed = msg[3] / 16 + offset + front_axle_speed
+        # The speed of the rear axle #1, left wheel speed, 
+        rear_left1_wheel_speed  = msg[4] / 16 + offset + front_axle_speed
+        # The speed of the rear axle #1, right wheel speed, 
+        rear_right1_wheel_speed = msg[5] / 16 + offset + front_axle_speed
+        # The speed of the rear axle #2, left wheel speed, 
+        rear_left2_wheel_speed  = msg[6] / 16 + offset + front_axle_speed
+        # The speed of the rear axle #2, right wheel speed, 
+        rear_right2_wheel_speed = msg[7] / 16 + offset + front_axle_speed
+
+        return(front_axle_speed, front_left_wheel_speed, front_right_wheel_speed, \
+                rear_left1_wheel_speed, rear_right1_wheel_speed, \
+                rear_left2_wheel_speed, rear_right2_wheel_speed)
+
+    def parse_gear(self, msg):
+        '''
+        Parse CAN msg (PGN 61445) to get gear info.
+
+        in: CAN msg
+        out: gear value
+            Range: -125 to +125, 
+            negative values are reverse gears, 
+            positive values are forward gears, 
+            zero is neutral. 
+            251 (0xFB) is park.
+        '''
+        offset = -125
+        gear_unint = msg[3]
+        if gear_unint == 251: # 0XFB
+            print('gear park')
+            pass
+        else:
+            gear_unint += offset
+        return (gear_unint,)
+
+    def parse_tachograph(self, msg):
+        '''
+        function: get Tachograph infomation.
+
+        in: CAN msg
+        out: Direction indicator, Tachograph vehicle speed
+        '''
+        direction_indicator = msg[3]
+        direction_indicator = direction_indicator >> 6  # get last 2 bits
+        tachograph_vehicle_speed = (msg[6] + msg[7] * 256) / 256
+        return (direction_indicator,tachograph_vehicle_speed)
+
+    def parse_wheel_speed(self, msg):
+        '''
+        Parse WHEEL_SPEEDS info from Toyota Corolla.
+        
+        in: CAN msg
+        out: in [km/h]
+            WHEEL_SPEED_FR
+            WHEEL_SPEED_FL
+            WHEEL_SPEED_RR
+            WHEEL_SPEED_RL
+        
+        dbc: MSB, unsigned
+            BO_ 170 WHEEL_SPEEDS: 8 XXX
+            SG_ WHEEL_SPEED_FR : 7|16@0+ (0.01,-67.67) [0|250] "kph" XXX
+            SG_ WHEEL_SPEED_FL : 23|16@0+ (0.01,-67.67) [0|250] "kph" XXX
+            SG_ WHEEL_SPEED_RR : 39|16@0+ (0.01,-67.67) [0|250] "kph" XXX
+            SG_ WHEEL_SPEED_RL : 55
+            |16@0+ (0.01,-67.67) [0|250] "kph" XXX
+        '''
+        offset = -67.67
+        speed_fr = (msg[0] * 256 + msg[1]) * 0.01 + offset
+        speed_fl = (msg[2] * 256 + msg[3]) * 0.01 + offset
+        speed_rr = (msg[4] * 256 + msg[5]) * 0.01 + offset
+        speed_rl = (msg[6] * 256 + msg[7]) * 0.01 + offset
+        return (speed_fr, speed_fl, speed_rr, speed_rl)
+
+    def parse_gear_carola(self, msg):
+        '''
+        Parse Gear info from Toyota Corolla.
+
+        in: CAN msg
+        out: Gear vaule
+
+        dbc: MSB, unsigned
+            SG_ Name : StartBit | Length @ ByteOrder SignedFlag (Factor,Offset) [Minimum | Maximum] "Unit" Receiver1,Receiver2
+            BO_ 956 GEAR_PACKET: 8 XXX
+            SG_ GEAR : 13|6@0+ (1,0) [0|63] "" XXX
+            SG_ SPORT_ON : 3|1@0+ (1,0) [0|1] "" XXX
+        '''
+        gear = msg[1]
+        return (gear,)
+
+if __name__ == '__main__':    
+    pass
+
--- can_reader.py
+++ can_reader.py
@@ -21,161 +21,10 @@ There are 2 ways to install python-can:
 
 import os
 import sys
-from enum import Enum
 import datetime
 from can.io.blf import BLFReader
 from can.io.asc import ASCReader
-
-
-class PGN(Enum):
-    SSI2       = 61481   # Slope Sensor Information 2
-    ARI        = 61482   # Angular Rate
-    ACCS       = 61485   # Acceleration Sensor
-    CCVS1      = 65265   # Cruise Control/Vehicle Speed 1, to get Wheel-Based Vehicle Speed
-    GEAR       = 61445   # 'Transmission Selected Gear' in 'Electronic Transmission Controller' message.
-    WSI        = 65215   # Wheel Speed Information   
-    TACHOGRAPH = 65132   # Tachograph
-
-class CanParser():
-    def __init__(self):
-        pass
-
-    def parse_PDU(self, PDU):
-        '''
-        function: get Priority, PGN, PF, PS, SA info from PDU.
-
-        in: int, PDU, eg 0x0cf029a2
-        '''
-        priority = (PDU >> 26) & 7  # 0b0111
-        PF = (PDU >> 16) & 255      # 0b11111111
-        PS = (PDU >> 8) & 255       # 0b11111111
-        SA = PDU & 255              # 0b11111111
-
-        if PF < 240:
-            PGN = PF * 256
-        else:
-            PGN = PF * 256 + PS
-        # print('{0},{1},{2},{3},{4},{5}'.format(hex(PDU), priority, PGN, PF, PS, SA))
-        return (priority, PGN, PF, PS, SA)
-
-    def parse_gyro(self, msg):
-        '''
-        Parse CAN msg to get Angular Rate data.
-
-        in: CAN msg
-        out: tuple, (gyro_x, gyro_y, gyro_z) in [deg/s]
-        '''
-        wx_uint = msg[0] + 256 * msg[1] 
-        wy_uint = msg[2] + 256 * msg[3] 
-        wz_uint =  msg[4] + 256 * msg[5] 
-        wx = wx_uint * (1/128.0) - 250.0
-        wy = wy_uint * (1/128.0) - 250.0
-        wz = wz_uint * (1/128.0) - 250.0
-        # print('WX: {0:3.2f} WY: {1:3.2f} WZ: {2:3.2f}'.format(wx,wy,wz))
-        return (wx, wy, wz)
-
-    def parse_accel(self, msg):
-        '''
-        Parse CAN msg to get Acceleration Sensor data.
-
-        in: CAN msg
-        out: tuple, (accel_x, accel_y, accel_z) in [m/s²]
-        '''
-        ax_uint = msg[0] + 256 * msg[1] 
-        ay_uint = msg[2] + 256 * msg[3] 
-        az_uint =  msg[4] + 256 * msg[5] 
-        ax = ax_uint * (0.01) - 320.0
-        ay = ay_uint * (0.01) - 320.0
-        az = az_uint * (0.01) - 320.0
-        # print('AX: {0:3.2f} AY: {1:3.2f} AZ: {2:3.2f}'.format(ax,ay,az))
-        return (ax, ay, az)
-
-    def parse_tilt(self, msg):
-        '''
-        Parse CAN msg to get tilt data.
-
-        in: CAN msg
-        out: tuple, (roll, pitch) in [deg]
-        '''
-        pitch_uint = msg[0] + 256 * msg[1] +  65536 * msg[2]
-        roll_uint = msg[3] + 256 * msg[4] +  65536 * msg[5]
-        pitch = pitch_uint * (1/32768) - 250.0
-        roll = roll_uint * (1/32768) - 250.0
-        # print('Roll: {0:3.2f} Pitch: {1:3.2f}'.format(roll,pitch))
-        return (roll, pitch)
-
-    def parse_velocity1(self, msg):
-        '''
-        Parse CAN msg (PGN 65265) to get Wheel-Based Vehicle Speed. Speed of the vehicle as calculated from wheel or tailshaft speed.
-
-        in: CAN msg
-        out: Wheel-Based Vehicle Speed in [km/h]
-        '''
-        vel_unint = msg[1] + 256 * msg[2] 
-        vel_km_perhr = vel_unint * (1/256.0)
-        vel_m_persec = vel_km_perhr / 3.6
-        # print('{0:3.2f}'.format(vel_km_perhr))
-        return (vel_km_perhr,)
-
-    def parse_velocity2(self, msg):
-        '''
-        Parse CAN msg (PGN 65215) to get Wheel Speed Information.
-
-        in: CAN msg
-        out: (front_axle_speed, front_left_wheel_speed, front_right_wheel_speed
-        rear_left1_wheel_speed, rear_right1_wheel_speed, rear_left2_wheel_speed,rear_right2_wheel_speed)
-        '''
-        offset = -7.8125 #km/h
-        # Front Axle Speed, the average speed of the two front wheels. range:[0, 250.996] km/h. 
-        front_axle_speed        = (msg[0] + 256 * msg[1]) / 256.0
-        # The speed of the front axle, left wheel speed, 
-        front_left_wheel_speed  = msg[2] / 16 + offset + front_axle_speed
-        # The speed of the front axle, right wheel speed, 
-        front_right_wheel_speed = msg[3] / 16 + offset + front_axle_speed
-        # The speed of the rear axle #1, left wheel speed, 
-        rear_left1_wheel_speed  = msg[4] / 16 + offset + front_axle_speed
-        # The speed of the rear axle #1, right wheel speed, 
-        rear_right1_wheel_speed = msg[5] / 16 + offset + front_axle_speed
-        # The speed of the rear axle #2, left wheel speed, 
-        rear_left2_wheel_speed  = msg[6] / 16 + offset + front_axle_speed
-        # The speed of the rear axle #2, right wheel speed, 
-        rear_right2_wheel_speed = msg[7] / 16 + offset + front_axle_speed
-
-        return(front_axle_speed, front_left_wheel_speed, front_right_wheel_speed, \
-                rear_left1_wheel_speed, rear_right1_wheel_speed, \
-                rear_left2_wheel_speed, rear_right2_wheel_speed)
-
-    def parse_gear(self, msg):
-        '''
-        Parse CAN msg (PGN 61445) to get gear info.
-
-        in: CAN msg
-        out: gear value
-            Range: -125 to +125, 
-            negative values are reverse gears, 
-            positive values are forward gears, 
-            zero is neutral. 
-            251 (0xFB) is park.
-        '''
-        offset = -125
-        gear_unint = msg[3]
-        if gear_unint == 251:
-            print('gear park')
-        gear_unint += offset
-        # print(gear_unint)
-        return (gear_unint,)
-
-    def parse_tachograph(self, msg):
-        '''
-        function: get Tachograph infomation.
-
-        in: CAN msg
-        out: Direction indicator, Tachograph vehicle speed
-        '''
-        direction_indicator = msg[3]
-        direction_indicator = direction_indicator >> 6  # get last 2 bits
-        tachograph_vehicle_speed = (msg[6] + msg[7] * 256) / 256
-        return (direction_indicator,tachograph_vehicle_speed)
+from can_parser import PGNType, CANParser
 
 class CanReader():
     '''
@@ -187,9 +36,18 @@ class CanReader():
         self.reader = None
         self.msg = None
 
+        self.log_file_all = None
+        self.log_file_gyro = None
+        self.log_file_accel = None
+        self.log_file_tilt = None
+        self.log_file_vel1 = None
+        self.log_file_vel2 = None
+        self.log_dir_gear = None
+        self.log_dir_tacho = None
+
         if file_name is not None:
             self.reader_factory()
-            self.create_log_files ()
+            self.create_log_files()
         pass
     
     def reader_factory(self):
@@ -245,50 +103,60 @@ class CanReader():
         self.log_file_all.flush()        
         pass
 
+    def close_log_files (self):
+        self.log_file_all.close()
+        self.log_file_gyro.close()
+        self.log_file_accel.close()
+        self.log_file_tilt.close()
+        self.log_file_vel1.close()
+        self.log_file_vel2.close()
+        self.log_dir_gear.close()
+        self.log_dir_tacho.close()
+        
     def parse_payload(self, msg):
         '''
         Parse CAN msg to get gyro/accel/tilt/velocity data.
 
         in: CAN msg
         '''
-        can_parser = CanParser()
+        can_parser = CANParser()
         (_Priority, _PGN, _PF, _PS, _SA) = can_parser.parse_PDU(msg.arbitration_id)
 
         data = None
         str = '{0:f},{1},{2},'.format(msg.timestamp, hex(msg.arbitration_id), _PGN)
 
-        if _PGN == PGN.SSI2.value:    # Slope Sensor Information 2
+        if _PGN == PGNType.SSI2.value:    # Slope Sensor Information 2
             data = can_parser.parse_tilt(msg.data)
             str += ','.join('{0:f}'.format(i) for i in data) + '\n'
             self.log_file_tilt.write(str )
             self.log_file_tilt.flush()
-        elif _PGN == PGN.ARI.value:   # Angular Rate
+        elif _PGN == PGNType.ARI.value:   # Angular Rate
             data = can_parser.parse_gyro(msg.data)
             str += ','.join('{0:f}'.format(i) for i in data) + '\n'
             self.log_file_gyro.write(str )
             self.log_file_gyro.flush()
-        elif _PGN == PGN.ACCS.value:  # Acceleration Sensor
+        elif _PGN == PGNType.ACCS.value:  # Acceleration Sensor
             data = can_parser.parse_accel(msg.data)
             str += ','.join('{0:f}'.format(i) for i in data) + '\n'
             self.log_file_accel.write(str )
             self.log_file_accel.flush()
-        elif _PGN == PGN.CCVS1.value: # Cruise Control/Vehicle Speed 1
+        elif _PGN == PGNType.CCVS1.value: # Cruise Control/Vehicle Speed 1
             data = can_parser.parse_velocity1(msg.data)
             str += ','.join('{0:f}'.format(i) for i in data) + '\n'
             self.log_file_vel1.write(str )
             self.log_file_vel1.flush()
-        elif _PGN == PGN.WSI.value: # Wheel Speed Information
+        elif _PGN == PGNType.WSI.value: # Wheel Speed Information
             data = can_parser.parse_velocity2(msg.data)
             str += ','.join('{0:f}'.format(i) for i in data) + '\n'
             self.log_file_vel2.write(str )
             self.log_file_vel2.flush()
-        elif _PGN == PGN.GEAR.value:
+        elif _PGN == PGNType.GEAR.value:
             data = can_parser.parse_gear(msg.data)
             str += ','.join('{0:f}'.format(i) for i in data) + '\n'
             self.log_dir_gear.write(str )
             self.log_dir_gear.flush()            
             pass
-        elif _PGN == PGN.TACHOGRAPH.value:
+        elif _PGN == PGNType.TACHOGRAPH.value:
             data = can_parser.parse_tachograph(msg.data)
             str += ','.join('{0:f}'.format(i) for i in data) + '\n'
             self.log_dir_tacho.write(str )
@@ -302,16 +170,135 @@ class CanReader():
             self.log_file_all.flush()
         pass
 
+class CANTestLogReader():
+    '''
+    Parse CAN messages from CANTestLog.
+    '''
+    def __init__(self, file_name):
+        self.file = file_name
+        self.gear = 0
+        self.log_file_all = None
+        self.log_file_vel = None
+        self.log_dir_gear = None
+        if file_name is not None:
+            self.create_log_files()
+        pass
 
-if __name__ == '__main__':
-    # file_name = sys.argv[1]
-    file_name = '/Users/songyang/project/analyze/drive_test/CNH/2020-5-27/data/C9-MY134_OBW1_B009_2020-05-22_15-47-02_25200kg_MS1_rural_motorway__begins_with_strong_uphill.blf'
+    def create_log_files (self):
+        '''
+        create log files.
+        '''
+        if not os.path.exists('data/'):
+            os.mkdir('data/')
+        start_time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+
+        file_dir        = os.path.join('data', start_time + '.csv')
+        file_dir_vel    = os.path.join('data', start_time + '_vel' + '.csv')
+        file_dir_gear   = os.path.join('data', start_time + '_gear' + '.csv')
+        self.log_file_all   = open(file_dir, 'w')
+        self.log_file_vel   = open(file_dir_vel, 'w')
+        self.log_dir_gear   = open(file_dir_gear, 'w')
+
+        print('Start logging: {0}'.format(file_dir))
+        header = 'time, time(sec), ID, payload'.replace(' ', '')
+        self.log_file_all.write(header + '\n')
+        self.log_file_all.flush()
+        header = 'time, time(sec), ID, payload'.replace(' ', '')
+        self.log_file_vel.write(header + '\n')
+        self.log_file_vel.flush()        
+        self.log_dir_gear.write(header + '\n')
+        self.log_dir_gear.flush()        
+        pass
+
+    def close_log_files (self):
+        self.log_file_all.close()
+        self.log_file_vel.close()
+        self.log_dir_gear.close()
+
+    def Read(self,):
+        can_parser = CANParser()
+        with open(self.file, 'r', encoding='utf-8') as f:
+            idx = 0
+            line = f.readline()
+            while line:
+                try:
+                    idx += 1
+                    # ignore the first line header.
+                    if idx == 1:
+                        line = f.readline()
+                        continue
+                    
+                    item = line.split(',')
+                    # get system time.
+                    sys_time = item[2]
+                    sys_time = sys_time.split(':')
+                    second = sys_time[2].split('.')
+                    sys_time = float(sys_time[0]) * 3600 + float(sys_time[1]) * 60 + float(second[0]) + float(second[1])/1e3 + float(second[2])/1e6
+                    
+                    # get frame id
+                    id = item[3]
+                    id = int(id, 16)
+
+                    # # get msg
+                    msg = item[7].split(' ')[0:-1]
+                    msg = [ int(p, 16) for p in msg]
+
+                    data = None
+                    s = '{0},{1},{2},'.format(item[2],sys_time,id)
+                    if id == 0XAA:  # 170
+                        data = can_parser.parse_wheel_speed(msg)
+                        s += ','.join('{0:f}'.format(i) for i in data)
+                        s += ',' + str(self.gear) + '\n'
+                        self.log_file_vel.write(s )
+                        self.log_file_vel.flush()
+                        # print(speed_fr, speed_fl, speed_rr, speed_rl) 
+                    elif id == 0X3BC:  # 956
+                        data = can_parser.parse_gear_carola(msg)
+                        s += ','.join('{0:d}'.format(i) for i in data) + '\n'
+                        self.log_dir_gear.write(s )
+                        self.log_dir_gear.flush()
+                        self.gear = data[0]
+                        # print(data)
+                    else: # unknown id
+                        pass
+
+                    if data is not None:
+                        self.log_file_all.write(s)
+                        self.log_file_all.flush()
+                    pass
+
+                    line = f.readline()
+
+                except Exception as e:
+                    print('Error at line {0} :{1}'.format(idx,e))
+
+
+def ReadFromBLFAndASC(file_name):
     can_reader = CanReader(file_name)
-
     try:
         for item in can_reader.reader:
             # print(item)
             data = can_reader.parse_payload(item)
+        can_reader.close_log_files()
     except Exception as e:
         print(e)
 
+def ReadFromCANTest(file_name):
+    '''
+    Read from CANTest Log file.
+        Index,Direction,Timesteamp,ID,Format,Type,Length,Data,
+        0,Receive,15:57:03.554.0,0x000001aa,Standard,Data,0x06,00 00 00 00 00 B1 ,
+        495,Receive,15:57:04.044.0,0x000003bc,Standard,Data,0x08,00 20 00 00 00 00 00 00 ,
+    '''
+    can_reader = CANTestLogReader(file_name)
+    can_reader.Read()
+    can_reader.close_log_files()
+
+if __name__ == '__main__':
+    # file_name = sys.argv[1]
+    file_name = '/Users/songyang/project/analyze/drive_test/CNH/2020-11-4/data/20201029_EMC_test_sensata_eurocargo/Logging_3.blf'
+    ReadFromBLFAndASC(file_name)
+
+    # Read and parse CAN-Test csv log.
+    # file_name = '/Users/songyang/project/code/github/logger/data/CAN-11-02/can_log.csv'
+    # ReadFromCANTest(file_name)
